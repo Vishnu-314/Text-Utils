8739324ad2b7277a03034510dc1f5500e2044925
Vishnu-314 <103477586+Vishnu-314@users.noreply.github.com>
2023-01-20 00:57:08 +0530

    Master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from "./components/Navbar"
 import TextForm from './components/TextForm'
